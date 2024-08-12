8a41f00437a5de878207189f195293424cacaeba
richardnv <richardnv@gmail.com>
2024-08-11 23:00:06 -0400

    GET author WIP

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,19 +40,52 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here  
-  return res.send(JSON.stringify(users));
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
+// This will always return an error "Not Found" 
+// because the "books" data in bookdb.js 
+// contains no ISBN property.
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    let isbn = req.params.isbn;  
+    if (isbn) {
+        let isbnbooks = books.filter((book) => book.isbn === isbn);
+        if (isbnbooks) {
+            return res.send(JSON.stringify(isbnbooks));
+        } else {
+            return res.send(JSON.stringify("Sory no book found with this ISBN ${isbn}!"));
+        }
+    } 
+    return res.send(JSON.stringify("message: Please supply an ISBN!"));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;    
+    let outmsg = "";
+    let booksByAuthor = [];
+    
+    if (author){            
+        let bookCount = books.length;    
+        for (let i = 1; i < bookCount; i++) {            
+            let auth = books[i].author;
+            let title = books[i].title;
+            if (auth === author){            
+                outmsg += "Author: " + auth + " Title: " + title + ", "
+            }
+            // if (book.author === author){
+            //     booksByAuthor.push(book);
+            // }
+        }  
+        //if (booksByAuthor.length > 0){            
+            return res.send(JSON.stringify(outmsg));           
+        // } else {
+        //     return res.send(JSON.stringify("No books found by author " + author + " !"));
+        // }
+    }    
+    return res.send(JSON.stringify("Please supply an author!"));
+ 
 });
 
 // Get all books based on title
